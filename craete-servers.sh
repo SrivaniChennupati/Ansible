e25b2f602fd3e4ce81c868a089ddbf3963547ba5
Srivani <SrivaniChennupati@gmail.com>
2023-09-05 20:05:07 -0500

    automate instances

--- craete-servers.sh
+++ craete-servers.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 Instancenames=("mongodb" "catalouge" "web" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payments" "dispatch")
-
+instance_Type=""
 for i in "${Instancenames[@]}"
 do
-echo "$i"
+if [[ $i == "mongodb" || $i == "mysql" ]]
+then
+instance_Type="t3.micro"
+else
+instance_Type="t2.micro"
+fi
+private_IpAdress=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_Type --security-group-ids sg-0512695d6d01b2c74|jq -r '.Instances[0].PrivateIpAddress')
+#echo "$i"
+
 done
