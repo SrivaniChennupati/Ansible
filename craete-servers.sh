217f37e134b25a3ac5ecde9f61f112c201a53cca
Srivani <SrivaniChennupati@gmail.com>
2023-09-05 20:08:27 -0500

    automate instances

--- craete-servers.sh
+++ craete-servers.sh
@@ -13,6 +13,7 @@ else
 instance_Type="t2.micro"
 fi
 private_IpAdress=$(aws ec2 run-instances --image-id $Imageid --instance-type $instance_Type --security-group-ids $Securitygroup_id|jq -r '.Instances[0].PrivateIpAddress')
+echo "Created $i : Ip Address : $private_IpAdress"
 #echo "$i"
 
 done
