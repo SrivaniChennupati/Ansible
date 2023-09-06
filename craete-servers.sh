7b45ab1fc8ea8aae7b91cbba54e904f5967c5778
Srivani <SrivaniChennupati@gmail.com>
2023-09-05 20:20:41 -0500

    automate instances

--- craete-servers.sh
+++ craete-servers.sh
@@ -4,16 +4,30 @@ Instancenames=("mongodb" "catalouge" "web" "redis" "user" "cart" "mysql" "shippi
 instance_Type=""
 Imageid=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
+domain_name=devopsvani.online
 for i in "${Instancenames[@]}"
 do
-if [[ $i == "mongodb" || $i == "mysql" ]]
-then
-instance_Type="t3.micro"
-else
-instance_Type="t2.micro"
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        instance_Type="t3.micro"
+    else
+        instance_Type="t2.micro"
 fi
-private_IpAdress=$(aws ec2 run-instances --image-id $Imageid --instance-type $instance_Type --security-group-ids $Securitygroup_id|jq -r '.Instances[0].PrivateIpAddress')
+echo "Creating $i....."
+private_IpAdress=$(aws ec2 run-instances --image-id $Imageid --instance-type $instance_Type --security-group-ids $Securitygroup_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"|jq -r '.Instances[0].PrivateIpAddress')
 echo "Created $i : Ip Address : $private_IpAdress"
+
+aws route53 change-resource-record-sets --hosted-zone-id Z00901702AI0X0PSLUZQF --change-batch '{
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$domain_name'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$private_IpAdress'"}]
+}}]
+}'
 #echo "$i"
 
 done
