68fc6dacdbb2008e3a42da291054ac074000d959
Srivani <SrivaniChennupati@gmail.com>
2023-09-05 21:57:46 -0500

    Installing Packages

--- craete-servers.sh
+++ craete-servers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#now i AM giving the names through command line
 
-Instancenames=("mongodb" "catalouge" "web" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payments" "dispatch")
+#Instancenames=("mongodb" "catalouge" "web" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payments" "dispatch")
+Instancenames=$@
 instance_Type=""
 Imageid=ami-03265a0778a880afb
 Securitygroup_id=sg-0512695d6d01b2c74
 domain_name=devopsvani.online
-for i in "${Instancenames[@]}"
+for i in $@
 do
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
@@ -18,7 +20,7 @@ private_IpAdress=$(aws ec2 run-instances --image-id $Imageid --instance-type $in
 echo "Created $i : Ip Address : $private_IpAdress"
 
 aws route53 change-resource-record-sets --hosted-zone-id Z00901702AI0X0PSLUZQF --change-batch '{
-            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Comment": "CREATE",
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
