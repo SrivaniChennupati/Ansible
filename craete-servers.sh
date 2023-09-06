c9b05263ddb495e14eadb91861f7d036b8e14530
Srivani <SrivaniChennupati@gmail.com>
2023-09-05 20:07:30 -0500

    automate instances

--- craete-servers.sh
+++ craete-servers.sh
@@ -2,6 +2,8 @@
 
 Instancenames=("mongodb" "catalouge" "web" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payments" "dispatch")
 instance_Type=""
+Imageid=ami-03265a0778a880afb
+Securitygroup_id=sg-0512695d6d01b2c74
 for i in "${Instancenames[@]}"
 do
 if [[ $i == "mongodb" || $i == "mysql" ]]
@@ -10,7 +12,7 @@ instance_Type="t3.micro"
 else
 instance_Type="t2.micro"
 fi
-private_IpAdress=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_Type --security-group-ids sg-0512695d6d01b2c74|jq -r '.Instances[0].PrivateIpAddress')
+private_IpAdress=$(aws ec2 run-instances --image-id $Imageid --instance-type $instance_Type --security-group-ids $Securitygroup_id|jq -r '.Instances[0].PrivateIpAddress')
 #echo "$i"
 
 done
